d81e3a0928647820dea0a76c1130464154f7543d
desteban <esteban28c@hotmail.com>
2022-04-14 19:08:15 -0500

    Updates

---
+++ component---src-pages-404-js-bedc2824f18f634545fd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),l=a(8311),u=a(9086);t.default=function(){return n.createElement(l.Z,null,n.createElement(u.pQ,{title:"404: Not found"}),n.createElement("div",{className:"center"},n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bedc2824f18f634545fd.js.map
